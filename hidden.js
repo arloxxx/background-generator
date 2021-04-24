5cce8d59b0262ee5b5c1d75152b8cd84dd0aea7a
unknown <mfsu2@yahoo.com>
2021-04-24 12:13:48 -0400

    adding files to starting project

---
+++ hidden.js
@@ -0,0 +1,9 @@
+
+const searchBox = document.querySelector('.searchBox')
+const expand = document.querySelector('.expandButton')
+
+expand.addEventListener('click', () => {
+    searchBox.classList.toggle('active')
+
+
+})
